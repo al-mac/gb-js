5142be8e3667739ab2f05f21655df8399e25a843
Alexandre Machado <al.mac@live.com>
2018-05-06 15:43:21 -0300

    test

--- web/service-worker.js
+++ web/service-worker.js
@@ -2,38 +2,37 @@ var dataCacheName = 'gameboy-v1';
 var cacheName = 'gameboyPWA-1';
 var filesToCache = [
   './index.html',
-  'web/homescreen192.png',
-  'web/index.css',
-  'web/index.js',
-  'emulator/apu.js',
-  'emulator/cpu.js',
-  'emulator/dasm.js',
-  'emulator/gpu.js',
-  'emulator/mbc.js',
-  'emulator/mmu.js',
-  'emulator/system.js',
-  'emulator/timer.js'
+  './web/homescreen192.png',
+  './web/index.css',
+  './web/index.js',
+  './emulator/apu.js',
+  './emulator/cpu.js',
+  './emulator/dasm.js',
+  './emulator/gpu.js',
+  './emulator/mbc.js',
+  './emulator/mmu.js',
+  './emulator/system.js',
+  './emulator/timer.js'
 ];
 
 self.addEventListener('install', function(e) {
-	try {
-		e.waitUntil(caches.open(cacheName).then(function(cache) {
-		return cache.addAll(filesToCache);
-		}));
-	} catch {}
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 self.addEventListener('activate', function(e) {
-	try {
-		e.waitUntil(
-			caches.keys().then(function(keyList) {
-				return Promise.all(keyList.map(function(key) {
-				if (key !== cacheName && key !== dataCacheName) {
-					return caches.delete(key);
-				}}));
-			})
-		);
-	}
-	catch {}  
-	return self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+ 
+  return self.clients.claim();
 });
